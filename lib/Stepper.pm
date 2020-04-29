68bedddb7fa330f15a7bd6a18cb79734492925d0
Patrick LeBoutillier <patrick.leboutillier@gmail.com>
2020-04-29 19:59:27 -0400

    stepper

--- lib/Stepper.pm
+++ lib/Stepper.pm
@@ -8,13 +8,28 @@ sub new {
     my $class = shift ;
     my $wclk = shift ;
     my $wrst = shift ;
-    my $bos = shift ;  # 7 wire bus
+    my $bos = shift ;  # 6 wire bus
     my $name = shift ;
 
+    my $wnrm1 = new WIRE() ;
+    my $nr = new NOT($wrst, $wnrm1) ;
+    my $wnco1 = new WIRE() ;
+    my $nc = new NOT($wclk, $wnco1) ;
+    my $wmsn = new WIRE() ;
+    my $o1 = new OR($wrst, $wnco1, $wmsn) ;
+    my $wmsnn = new WIRE() ;
+    my $o2 = new OR($wrst, $wclk, $wmsnn) ;
+    
+    my $wn12b = new WIRE() ;
+    my $wn12a = new WIRE() ;
+    my $s1 = new OR($wrst, $wn12b, $bos->wire(0)) ;
+    my $wm112 = new WIRE() ;
+    my $m1 = new MEMORY($wnrm1, $wsn, $wm112) ;
+
     # Foreach memory circuit, connect to the wires.
-    for (my $j = 0 ; $j < 8 ; $j++){
-        new MEMORY($bis->wire($j), $ws, $bos->wire($j), $j)  
-    }
+    #for (my $j = 0 ; $j < 8 ; $j++){
+    #    new MEMORY($bis->wire($j), $ws, $bos->wire($j), $j)  
+    #}
     
     my $this = {
         clk => $wclk,
