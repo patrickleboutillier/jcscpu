628670c521ff984c5581612720e904c662e47834
Patrick LeBoutillier <patrickl@croesus.com>
2020-05-14 17:38:37 -0400

    restore

--- go/pkg/parts/memory.go
+++ go/pkg/parts/memory.go
@@ -1,64 +1,32 @@
 package parts
 
+import (
+	g "github.com/patrickleboutillier/jcscpu/go/pkg/gates"
+)
+
 /*
-package MEMORY ;
-
-use strict ;
-use Gates ;
-
-
-sub new {
-    my $class = shift ;
-    my $wi = shift ;
-    my $ws = shift ;
-    my $wo = shift ;
-    my $name = shift ;
-
-    # Build the memory circuit, and record the wires.
-    my $wa = new WIRE() ;
-    my $wb = new WIRE() ;
-    # Setting power to 1 here is required to have an initial value of 0 in the memory!
-    my $wc = new WIRE(1) ;
-    my $g1 = new NAND($wi, $ws, $wa, "g1") ;
-    my $g2 = new NAND($wa, $ws, $wb, "g2") ;
-    my $g3 = new NAND($wa, $wc, $wo, "g3") ;
-    my $g4 = new NAND($wo, $wb, $wc, "g4") ;
-    
-    my $this = {
-        i => $wi,
-        s => $ws,
-        o => $wo,
-        m => $wc,
-        name => $name
-    } ;
-
-    bless $this, $class ;
-    return $this ;
-}
-
-
-sub o {
-    my $this = shift ;
-    return $this->{o} ;
-}
-
-
-sub m {
-    my $this = shift ;
-    return (! $this->{m}->power()) || 0 ;
-}
-
-
-sub show {
-    my $this = shift ;
-
-    my $i = $this->{i}->power() ;
-    my $s = $this->{s}->power() ;
-    my $o = $this->{o}->power() ;
-
-    return "M($this->{name})[$i/$s/$o]" ;
-}
-
-
-1 ;
+MEMORY
 */
+type Memory struct {
+	i, s, o, m *g.Wire
+}
+
+func NewMemory(wi *g.Wire, ws *g.Wire, wo *g.Wire) *Memory {
+	wa := g.NewWire()
+	wb := g.NewWire()
+	wc := g.NewWire()
+	// Setting power to 1 here is required to have an initial value of 0 in the memory!
+	wc.SetPower(true)
+	this := &Memory{wi, ws, wo, wc}
+
+	g.NewNAND(wi, ws, wa)
+	g.NewNAND(wa, ws, wb)
+	g.NewNAND(wa, wc, wo)
+	g.NewNAND(wo, wb, wc)
+
+	return this
+}
+
+func (this *Memory) GetM() bool {
+	return !this.m.GetPower()
+}
