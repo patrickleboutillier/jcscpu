6cd7c193cfd53c38a23544df0a6bb53bbd309bca
Patrick LeBoutillier <patrickl@croesus.com>
2020-05-19 17:40:01 -0400

    new files

---
+++ go/pkg/parts/clock_test.go
@@ -0,0 +1,139 @@
+package parts
+
+/*
+use strict ;
+use Test::More ;
+use Clock ;
+use File::Spec ;
+
+plan(tests => 38) ;
+
+# CLOCK
+my $wclk = new WIRE() ;
+my $wclke = new WIRE() ;
+my $wclks = new WIRE() ;
+my $c = new CLOCK($wclk, $wclke, $wclks, 2) ;
+my $wclkd = $c->clkd() ;
+is($c->qticks(), 0, "Clock starts with 0 completed qticks") ;
+is($c->ticks(), 0, "Clock starts with 0 completed ticks") ;
+
+eval {
+    $c->start(100) ;
+} ;
+if ($@){
+    like($@, qr/Max clock ticks/, "Clock stopped after max (2) ticks") ;
+}
+is($c->qticks(), 8, "Clock did 8 (+1) qticks") ;
+is($c->ticks(), 2, "Clock did 2 ticks") ;
+
+
+foreach my $mode (qw(gates loop)){
+    $CLOCK::MODE = $mode ;
+
+    # Test with infite Hz
+    my $wclk = new WIRE() ;
+    my $wclke = new WIRE() ;
+    my $wclks = new WIRE() ;
+    my $c = new CLOCK($wclk, $wclke, $wclks, 2) ;
+    $c->clkd() ;
+
+    eval {
+        $c->start() ;
+    } ;
+    if ($@){
+        like($@, qr/Max clock ticks/, "Clock stopped after max (2) ticks") ;
+    }
+    is($c->qticks(), 8, "Clock did 8 (+1) qticks") ;
+
+    # The with no maxticks
+    my $wclk = new WIRE() ;
+    my $wclke = new WIRE() ;
+    my $wclks = new WIRE() ;
+    my $c = new CLOCK($wclk, $wclke, $wclks) ;
+    my $wclkd = $c->clkd() ;
+
+    # Add a prehook to clk to grab interrupt at the right moment.
+    $wclk->prehook(sub { 
+        my $qt = $c->qticks() ;
+        die("INTERRUPT $qt") if $qt >= (2*4) ;
+    } ) ;
+    eval {
+        $c->start() ;
+    } ;
+    # Since we are interrupting the clock on a prehook, it will have done the next $qtick when we get the signal.
+    # So when we stop it will have done an extra qtick.
+     if ($@){
+        like($@, qr/INTERRUPT 9/, "Clock interrupted after 9 ticks") ;
+    }
+    is($c->qticks(), 9, "Clock did 9 qticks") ;
+}
+
+
+$wclk = new WIRE() ;
+$wclke = new WIRE() ;
+$wclks = new WIRE() ;
+$c = new CLOCK($wclk, $wclke, $wclks) ;
+map { $c->qtick() } (1..(2*4)) ;
+is($c->qticks(), 8, "Clock did 8 qticks manually using qtick()") ;
+
+
+$wclk = new WIRE() ;
+$wclke = new WIRE() ;
+$wclks = new WIRE() ;
+$c = new CLOCK($wclk, $wclke, $wclks) ;
+$c->tick(2) ;
+
+is($c->qticks(), 8, "Clock did 8 qticks") ;
+map { $c->qtick() } (1..4) ;
+is($c->qticks(), 12, "Clock did 12 qticks") ;
+$c->qtick() ;
+is($c->qticks(), 13, "Clock did 13 qticks") ;
+eval {
+    $c->tick() ;
+} ;
+if ($@){
+    like($@, qr/Can't tick mid-cycle/, "Clock can't tick mid-cycle") ;   
+}
+
+
+# More precise clock cycle, qtick by qtick.
+$wclk = new WIRE() ;
+$wclke = new WIRE() ;
+$wclks = new WIRE() ;
+$c = new CLOCK($wclk, $wclke, $wclks) ;
+$wclkd = $c->clkd() ;
+
+$c->qtick() ;
+is($wclk->power(),  1, "clk on") ;
+is($wclkd->power(), 0, "clkd off") ;
+is($wclke->power(), 1, "clke on") ;
+is($wclks->power(), 0, "clks off") ;
+$c->qtick() ;
+is($wclk->power(),  1, "clk on") ;
+is($wclkd->power(), 1, "clkd on") ;
+is($wclke->power(), 1, "clke on") ;
+is($wclks->power(), 1, "clks on") ;
+$c->qtick() ;
+is($wclk->power(),  0, "clk off") ;
+is($wclkd->power(), 1, "clkd on") ;
+is($wclke->power(), 1, "clke on") ;
+is($wclks->power(), 0, "clks off") ;
+
+# Suppress output for CLOCK::DEBUG coverage
+open my $olderr, '>&', \*STDERR or die $! ;
+open STDERR, File::Spec->devnull() ;
+$CLOCK::DEBUG = 1 ;
+$c->qtick() ;
+
+is($wclk->power(),  0, "clk off") ;
+is($wclkd->power(), 0, "clkd off") ;
+is($wclke->power(), 0, "clke off") ;
+is($wclks->power(), 0, "clks off") ;
+$c->qtick() ;
+is($wclk->power(),  1, "clk on") ;
+is($wclkd->power(), 0, "clkd off") ;
+is($wclke->power(), 1, "clke on") ;
+is($wclks->power(), 0, "clks off") ;
+
+open STDERR, '>&', $olderr or die $! ;
+*/
---
+++ go/pkg/parts/stepper_test.go
@@ -0,0 +1,82 @@
+package parts 
+
+/*
+use strict ;
+use Test::More ;
+use Clock ;
+use Stepper ;
+
+my $max_clock_ticks = 256 ;
+plan(tests => 11 + $max_clock_ticks*2) ;
+
+# STEPPER
+# First we need a clock
+my $wclk = new WIRE() ;
+my $wclke = new WIRE() ;
+my $wclks = new WIRE() ;
+my $c = new CLOCK($wclk, $wclke, $wclks) ;
+
+# First we make the stepper advance manually.
+my $bsteps = new BUS(7) ;
+my $s = new STEPPER($wclk, $bsteps) ;
+$s->show() ;
+
+is_deeply([$bsteps->power(), $s->step()], ["0000000", 0], "initial state, step 0") ;
+$c->tick() ;
+is_deeply([$bsteps->power(), $s->step()], ["1000000", 1], "step 1") ;
+$c->tick() ;
+is_deeply([$bsteps->power(), $s->step()], ["0100000", 2], "step 2") ;
+$c->tick() ;
+is_deeply([$bsteps->power(), $s->step()], ["0010000", 3], "step 3") ;
+$c->tick() ;
+is_deeply([$bsteps->power(), $s->step()], ["0001000", 4], "step 4") ;
+$c->tick() ;
+is_deeply([$bsteps->power(), $s->step()], ["0000100", 5], "step 5") ;
+$c->tick() ;
+is_deeply([$bsteps->power(), $s->step()], ["0000010", 6], "step 6") ;
+$c->tick() ;
+is_deeply([$bsteps->power(), $s->step()], ["1000000", 1], "step 1, auto reset") ;
+
+# Now with an automatic clock
+$wclk = new WIRE() ;
+$wclke = new WIRE() ;
+$wclks = new WIRE() ;
+$c = new CLOCK($wclk, $wclke, $wclks, 256) ;
+
+# First we make the stepper advance manually.
+$bsteps = new BUS(7) ;
+$s = new STEPPER($wclk, $bsteps) ;
+
+is($c->qticks(), 0, "Starting test at qtick 0") ;
+is($c->ticks(), 0, "Starting test at tick 0") ;
+is($bsteps->power(), "0000000", "initial state, step 7") ;
+# Get out of step 7 by doing one click.
+$c->tick() ;
+
+$wclk->prehook(sub{
+    my $v = shift ;
+    if ($v){
+        # Since ticks() has already been incremented to the next tick by the clock prehook, 
+        # Subtract 1 from it.
+        my $t = $c->ticks() - 1 ;
+        my $tmod6 = ($t % 6) ;
+        is($s->step(), $tmod6 + 1, "Step is $t") ; # steps os 1-6
+
+        # Check the matching power value
+        my @os = split(//, $bsteps->power()) ;
+        # Bit $t should be on, and it should the the only one one.
+        # If we set it to 0, we should have power = "0000000"
+        $os[$tmod6] = "0" ;
+        is(join('', @os), "0000000", "Proper step ($t) should be set (" . $bsteps->power() . ")") ;
+    }
+}) ;
+
+
+eval {
+    $c->start() ;
+} ;
+if ($@){
+    like($@, qr/Max clock ticks/, "Clock stopped after max ticks") ;
+}
+is($c->qticks(), 1024, "Clock did 1024 qticks") ;
+*/
